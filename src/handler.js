198574683b17289737b245334847a70f09346acb
arkur11 <arifkurnia1411@gmail.com>
2023-02-27 11:18:38 +0800

    Bookshelf API

--- src/handler.js
+++ src/handler.js
@@ -124,27 +124,27 @@ const getAllBooks = (request, h) => {
   
   };
 
-  const getBookById = (request, h) => {
-    const {bookId} = request.params;
-    const book = books.filter((book) => book.id === bookId)[0];
-  
-    if (book !== undefined) {
-      return {
-        status: 'success',
-        data: {
-          book,
-        },
-      };
-    }
-  
-    const response = h.response({
-      status: 'fail',
-      message: 'Buku tidak ditemukan',
-    });
-  
-    response.code(404);
-    return response;
-  };
+const getBookById = (request, h) => {
+  const { id } = request.params;
+  const book = books.filter((b) => b.id === id)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+
+  return response;
+};
 
 const editBookById = (request, h) => {
   const { id } = request.params;
